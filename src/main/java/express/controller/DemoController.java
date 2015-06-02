c91b51170b5d6570329b03b0c68c9f3dd700c94a
Zonghan Wu <a476678244@163.com>
2015-06-02 14:56:23 +0800

    verify spring function

--- src/main/java/express/controller/DemoController.java
+++ src/main/java/express/controller/DemoController.java
@@ -1,8 +1,14 @@
 package express.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+  @RequestMapping("/login")
+  public String login(@RequestParam("username") String username) {
+    return username;
+  }
 }
