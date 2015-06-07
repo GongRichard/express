62cb261d89001888182f9e1e8a2f559391e331f6
Zonghan Wu <a476678244@163.com>
2015-06-07 20:24:49 +0800

    upgrade controller

--- src/main/java/express/controller/UpgradeController.java
+++ src/main/java/express/controller/UpgradeController.java
@@ -2,12 +2,20 @@ package express.controller;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.mongodb.morphia.Datastore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import express.config.SpringMongoConfig;
 import express.entity.SequenceId;
 
 @RestController
@@ -15,6 +23,8 @@ public class UpgradeController {
 
   @Autowired
   private Datastore datastore;
+  
+  private MongoOperations mongoOperation;
 
   @RequestMapping("/upgrade")
   public String upgrade() {
@@ -23,6 +33,26 @@ public class UpgradeController {
   }
   
   private void createSequences() {
+    Query query = new Query();
+    List<SequenceId> sequences = this.mongoOperation.find(query, SequenceId.class);
+    if (sequences.isEmpty()) {
+      SequenceId sequenceUser = new SequenceId(SequenceId.SEQUENCE_USER, 0);
+      this.mongoOperation.save(sequenceUser);
+      SequenceId sequenceExpressItem = new SequenceId(SequenceId.SEQUENCE_EXPRESS_ITEM, 0);
+      this.mongoOperation.save(sequenceExpressItem);
+      SequenceId sequenceBill = new SequenceId(SequenceId.SEQUENCE_EXPRESS_BILL, 0);
+      this.mongoOperation.save(sequenceBill);
+    }
     
+  }
+  
+  @PostConstruct
+  public void init() {
+    if (this.mongoOperation == null) {
+      @SuppressWarnings("resource")
+      ApplicationContext ctx = new AnnotationConfigApplicationContext(
+          SpringMongoConfig.class);
+      this.mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
+    }
   }
 }
--- src/main/java/express/entity/SequenceId.java
+++ src/main/java/express/entity/SequenceId.java
@@ -5,11 +5,25 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "sequence")
 public class SequenceId {
+  
+  public static final String SEQUENCE_USER = "User";
+  
+  public static final String SEQUENCE_EXPRESS_ITEM = "ExpressItem";
+  
+  public static final String SEQUENCE_EXPRESS_BILL = "ExpressBill";
 
   @Id
   private String id;
 
   private long seq;
+
+  public SequenceId() {}
+
+  public SequenceId(String id, long seq) {
+    super();
+    this.id = id;
+    this.seq = seq;
+  }
 
   public String getId() {
     return id;
